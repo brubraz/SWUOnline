137e67de25f247cf12da2c86ab5ace6287e0b4b5
LanceVE <lancevanelzen@gmail.com>
2025-03-18 13:04:19 -0500

    mod

--- BanPlayer.php
+++ BanPlayer.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION["useruid"])) {
   exit;
 }
 $useruid = $_SESSION["useruid"];
-if ($useruid != "OotTheMonk" && $useruid != "love" && $useruid != "ninin" && $useruid != "Brubraz") {
+if ($useruid != "OotTheMonk" && $useruid != "love" && $useruid != "ninin" && $useruid != "Brubraz" && $useruid != "Mobyus1") {
   echo ("You must log in to use this page.");
   exit;
 }
